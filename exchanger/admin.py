8a2d12da6686b9e2f715913442cea302aed34781
sspinelli <sacha.bba@gmail.com>
2017-12-08 16:36:42 -0300

    exchanger models #1

--- exchanger/admin.py
+++ exchanger/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import Currency, Wallet, Transaction
 
-# Register your models here.
+admin.site.register(Currency)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
--- exchanger/models.py
+++ exchanger/models.py
@@ -1,6 +1,79 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils.translation import ugettext as _
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(_('Nombre'), max_length=255)
+    symbol = models.CharField(_('Símbolo monetario'), max_length=5)
+
+    class Meta:
+        verbose_name = "Moneda"
+        verbose_name_plural = "Monedas"
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+
+class Wallet(models.Model):
+    currency = models.ForeignKey(
+        Currency,
+        verbose_name=_('Moneda'),
+        related_name='wallets'
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('Dueño'),
+        related_name='wallets'
+    )
+
+    balance = models.FloatField(_('Balance actual'), default=0)
+
+    class Meta:
+        verbose_name = "Billetera"
+        verbose_name_plural = "Billeteras"
+
+    def __str__(self):
+        return self.currency.symbol + ' ' + str(
+            self.balance) + ' -> ' + self.user.username
+
+    def __unicode__(self):
+        return u'%s%s%s%s%s' % (
+            self.currency.symbol, ' ', str(
+                self.balance), ' -> ', self.user.username)
+
+
+class Transaction(models.Model):
+    amount = models.FloatField(_('Monto de la transacción'))
+
+    from_wallet = models.ForeignKey(
+        Wallet,
+        verbose_name=_('Billetera desde'),
+        related_name='transactions_made'
+    )
+
+    to_wallet = models.ForeignKey(
+        Wallet,
+        verbose_name=_('Billetera hasta'),
+        related_name='transactions_obtained'
+    )
+
+    class Meta:
+        verbose_name = "Transacción"
+        verbose_name_plural = "Transacciones"
+
+    def __str__(self):
+        from_user = ' From: ' + self.from_wallet.user.username
+        from_to = from_user + ' To: ' + self.to_wallet.user.username
+        return str(self.amount) + from_to
+
+    def __unicode__(self):
+        from_user = ' From: ' + self.from_wallet.user.username
+        from_to = from_user + ' To: ' + self.to_wallet.user.username
+        return u'%s%s%s' % (str(self.amount), ' ', from_to)
